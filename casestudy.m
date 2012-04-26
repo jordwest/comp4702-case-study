53a80f7e99b913e44e8d2a6887467efaac50c76d
Jordan West <jordwest@gmail.com>
2012-04-26 15:18:23 +1000

    successfully importing dataset

---
+++ casestudy.m
@@ -0,0 +1,9 @@
+% COMP4702 Case Study
+% Semester 1, 2012
+%    Jordan West
+
+% A Novel Kernel Method for Clustering
+% Francesco Camastra, Member, IEEE, and
+%       Alessandro Verri
+
+wisconsin = dataset('File', 'datasets/breast-cancer-wisconsin.data', 'format', '%s%d%d%d%d%d%s%d%d%d%d', 'Delimiter', ',');
